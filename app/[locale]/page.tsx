81013f9e97db0f32fef350b1f9cbeeb0a6e3c910
piyushrajsingh092 <piyushrajsingh092@gmail.com>
2026-06-20 19:55:06 +0530

    Revert "feat: implement premium glassmorphic landing page with interactive widgets and dark mode visuals"
    This reverts commit e9c38a9e883f0947419f0334ef82ac590e473868.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,26 @@
-import InteractiveLandingPage from "@/components/home/InteractiveLandingPage";
+import { useTranslations } from "next-intl";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import HeroSection from "@/components/home/HeroSection";
+import ImpactStats from "@/components/home/ImpactStats";
+import ProgramsSection from "@/components/home/ProgramsSection";
+import DonationCTA from "@/components/home/DonationCTA";
+import TrusteesSection from "@/components/home/TrusteesSection";
+import MembershipPreview from "@/components/home/MembershipPreview";
 
 export default function HomePage() {
   return (
-    <main>
-      <InteractiveLandingPage />
-    </main>
+    <>
+      <Navbar />
+      <main>
+        <HeroSection />
+        <ImpactStats />
+        <ProgramsSection />
+        <DonationCTA />
+        <MembershipPreview />
+        <TrusteesSection />
+      </main>
+      <Footer />
+    </>
   );
 }
